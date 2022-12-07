4d8944ef86f2eb5ec1eebeaf5b88031b0809980d
ae.whitemamba <ae.whitemamba@github.com>
2022-12-07 22:56:45 +0900

    숫자맞추기게임-연습

---
+++ main.js
@@ -0,0 +1,78 @@
+// 랜덤번호 지정
+// 유저가 번호를 입력한다 그리고 go라는 버튼을 누름
+// 만약에 유저가 랜덤번호를 맞추면, 맞췄습니다라고 띄우고
+// 랜덤번호가 < 유저번호 보다 작으면 Down
+// 랜덤번호가 < 유저반호보가 크면 Up
+// Reset버튼을 누르면 게임이 리셋된다
+// 5번의 기회를 다쓰면 게임이 끝난다 ( 더이상 추측불가, 버튼이 disable)
+// 유저가 1~100 범위 밖의 숫자를 입력하면 알려준다. 기회를 깎지 않는다.
+// 유저가 이미 입력한 숫자를 또 입력하면 알려준다. 기회를 깎지 않는다
+
+
+let computerNum = 0;
+let playButton = document.getElementById("play-button");
+let userInput = document.getElementById("user-input");
+let resultArea = document.getElementById("result-area");
+let resetButton = document.getElementById("reset-button");
+let chances = 5;
+let gameOver = false;
+let chanceArea = document.getElementById("chance-area");
+let history = []
+
+playButton.addEventListener("click", play);
+resetButton.addEventListener("click", reset )
+// 마우스 가져다 클릭하면 자동으로 지워지게 만들기
+userInput.addEventListener("focus", function(){
+  userInput.value = "";
+});
+
+function pickRandomNum(){
+  computerNum = Math.floor(Math.random()*100)+1; // Math.random(); random한 숫자를 뽑게 도와주는 함수, 0~1사이
+  console.log("정답", computerNum);
+}
+function play(){
+  let userValue = userInput.value;
+
+  if(userValue<1 || userValue>100){
+    resultArea.textContent = "1과 100 사이의 숫자만 입력해 주세요"
+    return;
+  }
+  if(history.includes(userValue)){
+    resultArea.textContent = "이미 입력한 숫자입니다. 다른 숫자를 입력해 주세요";
+    return;
+  }
+  chances --;
+  chanceArea.textContent = `남은 기회 : ${chances}번`;
+  console.log("chance", chances);
+
+  if(userValue < computerNum){
+    resultArea.textContent = "Up!"
+  }else if(userValue > computerNum){
+    resultArea.textContent = "Down!"
+  }else{
+    resultArea.textContent = "정답!"
+    gameOver = true;
+  }
+
+  history.push(userValue);
+  console.log(history);
+
+  if(chances < 1){
+    gameOver = true;
+  }
+  if(gameOver == true){
+    playButton.disabled = true;
+  }
+}
+
+function reset() {
+  // user input창이 깨끗하게 정리되고
+  userInput.value = "";
+  resultArea.textContent = "1~100까지 숫자 중 번호를 입력해보세요";
+  gameOver = false;
+  playButton.disabled = false;
+  chances = 5;
+  chanceArea.innerHTML = `남은 기회:${chances}`;
+  userValueList = [];
+}
+pickRandomNum();
